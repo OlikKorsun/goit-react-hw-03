bfa777b24d066bdd3666551652fe0438aead0454
OlikKorsun <145294542+OlikKorsun@users.noreply.github.com>
2024-04-15 02:50:25 +0300

    add form, search, contacts ets

--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -1,3 +1,14 @@
-export default function ContactList(){
-  
+import Contact from '../Contact/Contact'
+import css from './ContactList.module.css'
+
+export default function ContactList({ contacts, onDelete }) {
+    return (
+    <ul className={css.contacts}>
+      {contacts.map((contact) => (
+        <li className={css.contact} key={contact.id}>
+          <Contact data={contact} onDelete={onDelete} />
+        </li>
+      ))}
+    </ul>
+  );
 }
--- src/components/SearchBox/SearchBox.jsx
+++ src/components/SearchBox/SearchBox.jsx
@@ -1,3 +1,9 @@
-export default function SearchBox() {
-  
+import css from './SearchBox.module.css'
+export default function SearchBox({ value, onFilter }) {
+    return (
+        <div className={css.searchForm}>
+            <p className={css.labelFilter}>Find contacts by name</p>
+            <input className={css.inputSearch} type="text" value={value} onChange={(evt) => onFilter(evt.target.value)} />
+        </div>
+    )
 }
