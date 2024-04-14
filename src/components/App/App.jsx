395e3b10ef4507d859f6cf064335a2afcdf96bb3
OlikKorsun <145294542+OlikKorsun@users.noreply.github.com>
2024-04-14 22:07:01 +0300

    1

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+// import { useState } from 'react'
 import './App.module.css'
 import ContactForm from '../ContactForm/ContactForm'
 import SearchBox from '../SearchBox/SearchBox'
